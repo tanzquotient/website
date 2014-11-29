21cb1eda670be0a33bed4c149a01d1eae680e58c
Simon Wehrli <wehrli.simon@gmail.com>
2014-11-29 10:33:28 +0100

    add fabfile and requirements.txt to git
    NOTE: fabfile and settings exposes MySQL password used in development ->
    has to be excluded in separate file later

---
+++ fabfile.py
@@ -0,0 +1,34 @@
+from fabric.api import lcd, local
+from fabric.context_managers import env
+import os
+
+def deploy():
+    with lcd(os.path.dirname(env.real_fabfile)):
+        # git
+        local('git pull')
+        local('python manage.py migrate')
+        
+        restart()
+        
+def reset():
+    with lcd(os.path.dirname(env.real_fabfile)):
+        # git
+        local('git pull')
+        
+        local_reset()
+        restart()
+        
+def restart():
+    # restart gunicorn via supervisor
+    local('sudo supervisorctl restart tq_website')
+    
+    # restart webserver
+    local('sudo service nginx restart')
+        
+def local_reset():
+    with lcd(os.path.dirname(env.real_fabfile)):
+        local('mysql --user=root --password="eesseell" --execute="drop schema if exists tq_website; CREATE SCHEMA tq_website DEFAULT CHARACTER SET utf8mb4 ;"')
+        local('python manage.py migrate')
+        local('python manage.py fill')
+        local('python manage.py fill_functions')
+        local('python manage.py fill_faq')
