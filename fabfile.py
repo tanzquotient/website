163cbc6442ef74c66129bb96de7473a654c295a8
Simon Wehrli <wehrli.simon@gmail.com>
2014-11-29 10:49:59 +0100

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -19,11 +19,17 @@ def reset():
         restart()
         
 def restart():
+    # switch to user tq
+    local('su - tq')
+    
     # restart gunicorn via supervisor
     local('sudo supervisorctl restart tq_website')
     
     # restart webserver
     local('sudo service nginx restart')
+    
+    # switch back to first user
+    local('logout')
         
 def local_reset():
     with lcd(os.path.dirname(env.real_fabfile)):
