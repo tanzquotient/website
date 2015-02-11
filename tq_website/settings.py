69c6570b6912b4476d11878ccad17fedc7df5498
Simon Wehrli <wehrli.simon@gmail.com>
2015-02-11 08:55:38 +0100

    extract sensitive information to separat settings_local file
    (and change SECRET_KEY because it was pushed to github)

--- tq_website/settings.py
+++ tq_website/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP,\
-    MEDIA_ROOT, STATIC_ROOT
+    MEDIA_ROOT, STATIC_ROOT, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD,\
+    EMAIL_SUBJECT_PREFIX
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
@@ -19,20 +20,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '65nyt75%e(kjqswhz@di(uqx$k)*gm^uv@%n64^6yue1@_lwgu'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, "templates"),
 ) 
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    # with . at beginning allows domain and subdomains
+    # with . at end allows FQDN
+    '.tq.ethz.ch.',  
+    '.tq.vseth.ch.',
+    '.tq.vseth.ethz.ch.',
+    '.tanzquotient.vseth.ethz.ch.',
+]
 
 # Application definition
 
@@ -66,26 +65,12 @@ ROOT_URLCONF = 'tq_website.urls'
 
 WSGI_APPLICATION = 'tq_website.wsgi.application'
 
-ALLOWED_HOSTS = [
-    # with . at beginning allows domain and subdomains
-    # with . at end allows FQDN
-    '.tq.ethz.ch.',  
-    '.tq.vseth.ch.',
-    '.tq.vseth.ethz.ch.',
-    '.tanzquotient.vseth.ethz.ch.',
-]
+EMAIL_HOST = 'mailsrv.vseth.ethz.ch'
+EMAIL_HOST_USER = 'anmeldungen@tq.vseth.ethz.ch'
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'NAME': 'tq_website',
-        'USER': 'root',
-        'PASSWORD': 'eesseell',
-    }
-}
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_SUBJECT_PREFIX = u"[TQ System] "
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -197,3 +182,6 @@ LOGGING = {
         },
     }
 }
+
+# import local settings (includes secrets, thats why settings_local MUST NOT BE UNDER VERSION CONTROL!!!)
+from settings_local import *
