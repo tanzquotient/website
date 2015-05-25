849020d749bc17404ec48687129ca0df29268e90
Simon Wehrli <wehrli.simon@gmail.com>
2015-05-25 18:46:23 +0200

    add missing migration for meta options

---
+++ courses/migrations/0012_auto_20150525_1844.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('courses', '0011_auto_20150522_1409'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='irregularlesson',
+            options={'ordering': ['date', 'time_from']},
+        ),
+        migrations.AlterModelOptions(
+            name='regularlessoncancellation',
+            options={'ordering': ['date']},
+        ),
+    ]
