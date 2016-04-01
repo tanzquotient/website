df2ffda5d514a7ae8b08abec277e01966388cced
Martin Zellner <martin.zellner@martinzellner.eu>
2016-04-01 23:10:49 +0200

    USI Validator

--- counterpayment/forms.py
+++ counterpayment/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from courses.models import Subscribe
+
+def validate_usi_exists(value):
+    if not Subscribe.objects.filter(usi=value).count() > 0:
+        raise ValidationError('The specified USI does not exist')
 
 class USIForm(forms.Form):
-    usi = forms.CharField(max_length=6, label="Unique Course Identifier (USI)")
+    usi_validator = RegexValidator(regex='[a-zA-Z0-9]{6}', message="Please enter a valid USI")
+    usi = forms.CharField(max_length=6, label="Unique Course Identifier (USI)", validators=[usi_validator, validate_usi_exists, ])
+
