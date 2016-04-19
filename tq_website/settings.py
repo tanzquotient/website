978d2dac752299baf7469239fbd60d885b89fd27
Martin Zellner <martin.zellner@martinzellner.eu>
2016-04-19 10:43:31 +0200

    fixed settings

--- tq_website/settings.py
+++ tq_website/settings.py
@@ -343,7 +343,6 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false', ],
             'class': 'django.utils.log.AdminEmailHandler'
         },
         'null': {
