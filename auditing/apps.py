591669c9c663fb88a437e01dbb5955edce9b1e86
Simon Wehrli <wehrli.simon@gmail.com>
2016-04-25 18:15:07 +0200

    cleanup
    remove unused app auditing
    remove several fill apps (should be made with fixtures instead or just load partail backup of database)

--- auditing/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuditingConfig(AppConfig):
-    name = 'auditing'
-    verbose_name = "Auditing"
-
-    def ready(self):
-        from auditing import signals
