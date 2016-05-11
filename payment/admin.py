1f298c956a439aab1139f1e23ad09cbee5fea349
Martin Zellner <martin.zellner@martinzellner.eu>
2016-05-11 22:13:13 +0200

    added PF reference number

--- payment/admin.py
+++ payment/admin.py
@@ -5,7 +5,7 @@ from payment.models import *
 
 # Register your models here.
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'date', 'address', 'iban', 'bic', 'amount', 'currency_code', 'remittance_user_string', 'state', 'subscription', 'filename']
+    list_display = ['id', 'name', 'date', 'address', 'transaction_id', 'iban', 'bic', 'amount', 'currency_code', 'remittance_user_string', 'state', 'subscription', 'filename']
 
 
 # Register your models here.
