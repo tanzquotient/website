c9290b5348ad769df8efcec54fd87e8225f8ded9
Martin Zellner <martin.zellner@martinzellner.eu>
2016-07-02 15:19:40 +0200

    changed reversion import for compatibility with version 1.10

--- courses/models.py
+++ courses/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 import datetime
 from django.utils.translation import ugettext as _
-import reversion
+from reversion import revisions as reversion
 
 from django.conf import settings
 
