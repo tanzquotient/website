1b92ad013fddb6a563f5411d53fcd447c6ed0fa0
Simon Wehrli <wehrli.simon@gmail.com>
2016-09-26 12:37:06 +0200

    change order of courses in course payment

--- payment/forms.py
+++ payment/forms.py
@@ -43,8 +43,8 @@ class USIForm(forms.Form):
 class VoucherForm(forms.Form):
     voucher_code = forms.CharField(max_length=6, label=_("Voucher Code"), validators=[voucher_valid, ])
 
-class CourseForm(forms.Form):
 
+class CourseForm(forms.Form):
     def __init__(self, user, *args, **kwargs):
         courses = []
         if user is not None:
@@ -53,6 +53,10 @@ class CourseForm(forms.Form):
                 courses = Course.objects.all()
             else:
                 courses = [teach.course for teach in Teach.objects.filter(teacher=user).all()]
+        courses = list(courses)
+        courses.sort(
+            key=lambda course: course.get_period().date_from if course.get_period() and course.get_period().date_from else datetime.date(year=1990, month=
+            1, day=1), reverse=True)
         super(CourseForm, self).__init__(*args, **kwargs)
-        self.fields['course'] = forms.ChoiceField(label=_("Select Course"), choices=[(course.id, course) for course in courses])
-
+        self.fields['course'] = forms.ChoiceField(label=_("Select Course"),
+                                                  choices=[(course.id, course) for course in courses])
