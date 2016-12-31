e1f6819488505ee03530857164d5b607dcfa9de3
Simon Wehrli <wehrli.simon@gmail.com>
2016-12-31 15:04:14 +0100

    add memcached cache

--- survey/views.py
+++ survey/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import get_object_or_404
 import re
 import logging
 from django.utils.translation import ugettext as _
-from django.core.cache import cache
 
 log = logging.getLogger('tq')
 
--- tq_website/settings.py
+++ tq_website/settings.py
@@ -452,6 +452,10 @@ LOCALE_PATHS = [
 
 CACHES = {
     'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'memcached:{}'.format(os.environ.get("TQ_MEMCACHED_PORT", '')),
+    },
+    'db': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
         'LOCATION': 'tq_cache_table',
     }
