f070642b4659f37bce49a481848d347aef9f1c1d
Simon Wehrli <wehrli.simon@gmail.com>
2017-01-26 13:48:47 +0100

    make docker-compose files consistent, add port hard-coded as default
    hard-coded because for some reason is not in environement (also when .env is specified in docker-compose, as it is now

--- tq_website/settings.py
+++ tq_website/settings.py
@@ -454,7 +454,7 @@ LOCALE_PATHS = [
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': 'memcached:{}'.format(os.environ.get("TQ_MEMCACHED_PORT", '')),
+        'LOCATION': 'memcached:{}'.format(os.environ.get("TQ_MEMCACHED_PORT", '11211')),
     },
     'db': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
