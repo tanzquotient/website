b70bf7da437878959309d720567d226f3abeae93
Simon Wehrli <wehrli.simon@gmail.com>
2017-04-05 15:31:26 +0200

    allow multiple users for one organisational function

--- organisation/admin.py
+++ organisation/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from organisation.models import Function
 
+class UserInline(admin.TabularInline):
+    model = Function.users.through
+    raw_id_fields = ('user',)
+    extra = 0
 
 @admin.register(Function)
 class FunctionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'active', 'user',)
+    list_display = ('name', 'email', 'active', 'format_users',)
+    inlines = (UserInline,)
+    exclude = ('users',)
--- organisation/models.py
+++ organisation/models.py
@@ -4,12 +4,13 @@ from django.conf import settings
 
 from . import managers
 
+
 # Create your models here.
 class Function(models.Model):
     name = models.CharField(max_length=255)
     active = models.BooleanField(default=True)
     email = models.EmailField(blank=True, null=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='functions')
 
     # position field for ordering columns (grappelli feature)
     position = models.PositiveSmallIntegerField("Position", default=0)
@@ -19,5 +20,15 @@ class Function(models.Model):
 
     objects = managers.FunctionManager()
 
+    def format_users(self):
+        return ', '.join(map(str, self.users.all()))
+
+    format_users.short_description = "Usernames"
+
+    def format_users_full_name(self):
+        return ', '.join([user.get_full_name() for user in self.users.all()])
+
+    format_users_full_name.short_description = "Users"
+
     def __str__(self):
-        return"{}".format(self.name)
+        return "{}".format(self.name)
