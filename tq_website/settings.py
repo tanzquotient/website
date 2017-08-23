tanzquotient/website

commit a154238dbc66629d2ba907b98e506e0c915590cb

Solved a part of issue #73: English spell checking is now working. Tried to install the ckeditor language plugin to allow selection of language, but the button is not displayed...

tq_website/settings.py
@@ -215,13 +215,19 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = 'pillow'
 
 CKEDITOR_SETTINGS = {
+    'disableNativeSpellChecker': False,
     'language': 'en',
+    'extraPlugins': 'language',
+    'language_list': ['en:English', 'de:Deutsch', 'fr:Français'],
     'toolbar_HTMLField': [
         ['Undo', 'Redo'],
         ['ShowBlocks'],
         ['Format', 'Styles'],
         ['Link', 'Unlink'],
         ['Source', ],
+    ],
+    'toolbar_CMS': [
+        ['language']
     ],
     'skin': 'moono',
 }

static/djangocms_text_ckeditor/language/lang/ar.js
@@ -1,0 +1,9 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.plugins.setLang( 'language', 'ar', {
+	button: 'حدد اللغة',
+	remove: 'حذف اللغة'
+} );

static/djangocms_text_ckeditor/language/lang/az.js
@@ -1,0 +1,9 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.plugins.setLang( 'language', 'az', {
+	button: 'Dilini təyin et',
+	remove: 'Dilini sil'
+} );

static/djangocms_text_ckeditor/language/lang/bg.js
@@ -1,0 +1,9 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.plugins.setLang( 'language', 'bg', {
+	button: 'Задай език',
+	remove: 'Премахни език'
+} );

static/djangocms_text_ckeditor/language/lang/ca.js
@@ -1,0 +1,9 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.plugins.setLang( 'language', 'ca', {
+	button: 'Definir l\'idioma',
+	remove: 'Eliminar idioma'
+} );

static/djangocms_text_ckeditor/language/lang/cs.js
@@ -1,0 +1,9 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.plugins.setLang( 'language', 'cs', {
+	button: 'Nastavit jazyk',
+	remove: 'Odstranit jazyk'
+} );

commit 3eca7108bd093f9c42b7940748f38f5ab54048ea

Fixed missing toolbar for chk_editor (bug introduced in commit a154238dbc66629d2ba907b98e506e0c915590cb)

tq_website/settings.py
@@ -226,9 +226,6 @@ CKEDITOR_SETTINGS = {
         ['Link', 'Unlink'],
         ['Source', ],
     ],
-    'toolbar_CMS': [
-        ['language']
-    ],
     'skin': 'moono',
 }
 