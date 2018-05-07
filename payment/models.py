0df2e40f3552ff7bc5c8fe3b2899f86599ff6d85
kadirakin85 <aakin@phys.ethz.ch>
2018-05-07 19:19:19 +0200

    Migrations are done for payment status change. Now Archieve is an option for payment status

--- payment/models.py
+++ payment/models.py
@@ -15,8 +15,9 @@ class Payment(models.Model):
         MANUAL = 'manual'
         MATCHED = 'matched'
         PROCESSED = 'processed'
-
-        CHOICES = ((NEW, 'new'), (MANUAL, 'manual'), (MATCHED, 'matched'), (PROCESSED, 'processed'))
+        ARCHIVE = 'archive'
+        
+        CHOICES = ((NEW, 'new'), (MANUAL, 'manual'), (MATCHED, 'matched'), (PROCESSED, 'processed'), (ARCHIVE, 'archive'))
 
     class Type:
         SUBSCRIPTION_PAYMENT = 'subscription_payment'
@@ -24,13 +25,12 @@ class Payment(models.Model):
         COURSE_PAYMENT_TRANSFER = 'course_payment_transfer'
         IRRELEVANT = 'irrelevant'
         UNKNOWN = 'unknown'
-        ARCHIVE = 'archive'
+
 
         CHOICES = ((SUBSCRIPTION_PAYMENT, 'subscription payment'),
                    (SUBSCRIPTION_PAYMENT_TO_REIMBURSE, 'subscription payment (to reimburse)'),
                    (COURSE_PAYMENT_TRANSFER, 'course payment transfer'), (IRRELEVANT, 'irrelevant'),
-                   (UNKNOWN, 'unknown'),
-                   (ARCHIVE, 'archive'))
+                   (UNKNOWN, 'unknown'))
 
     class CreditDebit:
         UNKNOWN = 'unknown'
