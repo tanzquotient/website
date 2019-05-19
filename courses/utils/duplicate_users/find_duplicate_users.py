1512d23284256c391cecca88bb00028aecb678f9
Daniel Sparber <daniel.sparber@outlook.com>
2019-05-19 20:14:47 +0200

    Find duplicates by name

--- courses/utils/duplicate_users/find_duplicate_users.py
+++ courses/utils/duplicate_users/find_duplicate_users.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+import pytz
 from django.contrib.auth.models import User
 
 
@@ -20,9 +23,16 @@ def _find_duplicates_of(user):
     """
     Finds duplicates of user. Returns a list of ids including user's id, ordered by last login user joined the system.
     """
-    if user.email is None or user.email == "":
-        return list()
+    candidates = list()
 
-    candidates = list(User.objects.filter(email=user.email).order_by('-last_login'))
+    if user.email is not None and user.email != "":
+        candidates += list(User.objects.filter(email=user.email))
+
+    candidates += list(User.objects.filter(first_name=user.first_name, last_name=user.last_name))
+    # Some user accidentally switch first and last name
+    candidates += list(User.objects.filter(first_name=user.last_name, last_name=user.first_name))
+
+    candidates.sort(key=lambda u: u.last_login or datetime.min.replace(tzinfo=pytz.UTC), reverse=True)
+
     # candidates intentionally also includes the passed user
     return list({u.id for u in candidates})
