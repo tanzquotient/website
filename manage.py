028c0819bcb5d91e5efc581bfc6ff67efac66ea8
Daniel Sparber <daniel.sparber@outlook.com>
2019-07-09 21:56:35 +0200

    Reverted unwanted import

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import os
 import sys
 
-import env_file
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tq_website.settings")
-    env_file.load()
 
     from django.core.management import execute_from_command_line
 
