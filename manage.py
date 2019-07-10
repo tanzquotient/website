7cc6da2e0f3f12cd25924c4cdb9e8e8e6db8d290
Daniel Sparber <daniel.sparber@outlook.com>
2019-07-10 22:10:42 +0200

    Translations

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
 
+import env_file
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tq_website.settings")
+    env_file.load()
 
     from django.core.management import execute_from_command_line
 
