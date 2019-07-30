ce8b687814c97cee24584ea9a0bef9747f124339
Daniel Sparber <daniel.sparber@outlook.com>
2019-07-30 09:07:34 +0200

    Sort lessons without offering by first lesson date

--- courses/views.py
+++ courses/views.py
@@ -93,6 +93,8 @@ def course_list(request, subscription_type="all", style_name="all", force_previe
     # Courses without offering -> create fake offering
     courses_without_offering = list(filter(matches_filter, services.get_upcoming_courses_without_offering()))
     if courses_without_offering:
+        courses_without_offering.sort(key=lambda c: c.get_first_lesson_date())
+
         c_offerings.insert(0, {
             'offering': {
                 'name': _("Upcoming courses"),
