c8005574363a4b3a379c592379e1e8b2b30fbfd1
Daniel Sparber <daniel@sparber.io>
2020-01-27 16:37:13 +0100

    Load dotenv file if enviroment variables not set

--- manage.py
+++ manage.py
@@ -5,6 +5,10 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tq_website.settings")
 
+    if "TQ_DEBUG" not in os.environ.keys():
+        from dotenv import load_dotenv
+        load_dotenv()
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
