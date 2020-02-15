f48fe6a0c3a1a344aad7aef37019251ac4be6e2a
Daniel Sparber <daniel@sparber.io>
2020-02-15 15:49:09 +0100

    Fixed event copy

--- events/admin_actions.py
+++ events/admin_actions.py
@@ -1,11 +1,26 @@
 import datetime
 
+from events.models import Event
+
 
 def copy_event(modeladmin, request, queryset):
     for event in queryset:
-        event.name = event.name + " (Copy)"
+        old_id = event.id
         event.date = event.date + datetime.timedelta(days=7)
         event.id = None
+        event.save()
+        old_event = Event.objects.get(id=old_id)
+
+        event.set_current_language('de')
+        old_event.set_current_language('de')
+        event.name = old_event.name
+        event.description = old_event.description
+
+        event.set_current_language('en')
+        old_event.set_current_language('en')
+        event.name = old_event.name
+        event.description = old_event.description
+
         event.save()
 
 
