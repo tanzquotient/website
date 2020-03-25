9a61008c9390aff8be809b4ab242c842393e3b26
Daniel Sparber <daniel@sparber.io>
2020-03-25 11:17:28 +0100

    Updated sql script

--- scripts/sql/Subscriptions.sql
+++ scripts/sql/Subscriptions.sql
@@ -4,21 +4,23 @@ SELECT DISTINCT
     profile.phone_number,
     user.email,
     course.name as "Course name",
-    subscribe.usi /*,
-    subscribe.state,
-    subscribe.paymentmethod*/
-
+    subscribe.usi,
+    IFNULL(payment.amount, 0) as "paid",
+    subscribe.price_to_pay,
+    (subscribe.price_to_pay - IFNULL(payment.amount, 0)) as diff
 
 FROM courses_subscribe subscribe
-         JOIN courses_course course on subscribe.course_id = course.id
-         JOIN courses_offering offering on course.offering_id = offering.id
-         JOIN courses_period period on offering.period_id = period.id
-         JOIN courses_userprofile profile on subscribe.user_id = profile.user_id
-         JOIN auth_user user on profile.user_id = user.id
+    JOIN courses_course course on subscribe.course_id = course.id
+    JOIN courses_offering offering on course.offering_id = offering.id
+    JOIN courses_period period on offering.period_id = period.id
+    JOIN courses_userprofile profile on subscribe.user_id = profile.user_id
+    JOIN auth_user user on profile.user_id = user.id
+    LEFT JOIN payment_subscriptionpayment payment on subscribe.id = payment.subscription_id
+    LEFT JOIN payment_payment bank_transaction on payment.payment_id = bank_transaction.id
 
-WHERE
-        offering.name = 'FS2020 Q1' AND
-    /*subscribe.state NOT IN ('rejected')*/
-        subscribe.state = 'payed'
+WHERE offering.name = 'FS2020 Q1'
+    AND subscribe.state NOT IN ('rejected')
+    AND (paymentmethod IS NULL OR  paymentmethod <> 'voucher')
+    AND (subscribe.price_to_pay - IFNULL(payment.amount, 0)) <> 0
 
-ORDER BY user.first_name, user.last_name
+ORDER BY diff, user.first_name, user.last_name
