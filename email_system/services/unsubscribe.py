6e7d1eeb4f8486ebcc3aabe67e22807f332c2f67
Daniel Sparber <daniel@sparber.io>
2020-04-22 22:35:03 +0200

    Check code

--- email_system/services/unsubscribe.py
+++ email_system/services/unsubscribe.py
@@ -2,9 +2,10 @@ from email_system.models import UnsubscribeCode
 from groups.definitions import GroupDefinitions
 
 def unsubscribe(context, user_id, code):
-    unsubscribe_code = UnsubscribeCode.objects.filter(user_id=user_id)
+    unsubscribe_code = UnsubscribeCode.objects.filter(user_id=user_id, code=code)
     if not unsubscribe_code.exists():
         return False
+    unsubscribe_code = unsubscribe_code.first()
 
     if context == GroupDefinitions.TEST.name:
         profile = unsubscribe_code.user.profile
