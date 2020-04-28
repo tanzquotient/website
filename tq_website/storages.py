5f86c7e2f34bd746c89915317c431b64e1a01cca
Daniel Sparber <daniel@sparber.io>
2020-04-28 21:03:14 +0200

    Fixed storages

--- tq_website/storages.py
+++ tq_website/storages.py
@@ -4,14 +4,15 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class TqStorage(S3Boto3Storage):
 
-    def __init__(self, bucket_name, host, port, use_ssl, access_key, secret_key):
+    def __init__(self, bucket_name, host, port, region, use_ssl, custom_domain, access_key, secret_key):
         config = dict(
             use_ssl=use_ssl,
             access_key=access_key,
             secret_key=secret_key,
+            region_name=region
         )
-        if settings.S3_CUSTOM_DOMAIN:
-            config['custom_domain'] = settings.S3_CUSTOM_DOMAIN.format(bucket_name)
+        if custom_domain:
+            config['custom_domain'] = custom_domain.format(bucket_name)
         else:
             protocol = 'https' if use_ssl else 'http'
             config['endpoint_url'] = "{}://{}:{}".format(protocol, host, port)
@@ -24,7 +25,9 @@ class MediaStorage(TqStorage):
             bucket_name=settings.S3_MEDIA_BUCKET,
             host=settings.S3_MEDIA_HOST,
             port=settings.S3_MEDIA_PORT,
+            region=settings.S3_MEDIA_REGION,
             use_ssl=settings.S3_MEDIA_USE_SSL,
+            custom_domain=settings.S3_MEDIA_CUSTOM_DOMAIN,
             access_key=settings.S3_MEDIA_ACCESS_KEY,
             secret_key=settings.S3_MEDIA_SECRET_KEY,
         )
@@ -36,7 +39,9 @@ class StaticStorage(TqStorage):
             bucket_name=settings.S3_STATIC_BUCKET,
             host=settings.S3_STATIC_HOST,
             port=settings.S3_STATIC_PORT,
+            region=settings.S3_STATIC_REGION,
             use_ssl=settings.S3_STATIC_USE_SSL,
+            custom_domain=settings.S3_STATIC_CUSTOM_DOMAIN,
             access_key=settings.S3_STATIC_ACCESS_KEY,
             secret_key=settings.S3_STATIC_SECRET_KEY,
         )
@@ -48,7 +53,9 @@ class PostfinanceStorage(TqStorage):
             bucket_name=settings.S3_POSTFINANCE_BUCKET,
             host=settings.S3_POSTFINANCE_HOST,
             port=settings.S3_POSTFINANCE_PORT,
+            region=settings.S3_POSTFINANCE_REGION,
             use_ssl=settings.S3_POSTFINANCE_USE_SSL,
+            custom_domain=settings.S3_POSTFINANCE_CUSTOM_DOMAIN,
             access_key=settings.S3_POSTFINANCE_ACCESS_KEY,
             secret_key=settings.S3_POSTFINANCE_SECRET_KEY,
         )
