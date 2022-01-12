2ea3ae8eae6aa4126ba5db7f2ca26e0a9c59c787
Thore Goebel <thgoebel@student.ethz.ch>
2022-01-12 14:05:03 +0100

    Make sed idempotent
    Without this, a change in IP assigned by Docker would not be caught without deleting the overrides.yml first.

--- scripts/initialize_project.sh
+++ scripts/initialize_project.sh
@@ -15,9 +15,9 @@ docker-compose build
 # from the perspective of your browser accessing Minio.
 echo "Setting IPs for Minio..."
 ip=$(docker inspect tq-data | grep IPAddress | grep --extended-regexp --only-matching "([0-9]{1,3}\.){3}[0-9]{1,3}")
-sed -i "s/TQ_S3_MEDIA_HOST: overrideme/TQ_S3_MEDIA_HOST: $ip/" overrides.yml
-sed -i "s/TQ_S3_STATIC_HOST: overrideme/TQ_S3_STATIC_HOST: $ip/" overrides.yml
-sed -i "s/TQ_S3_POSTFINANCE_HOST: overrideme/TQ_S3_POSTFINANCE_HOST: $ip/" overrides.yml
+sed -i "s/TQ_S3_MEDIA_HOST: .*/TQ_S3_MEDIA_HOST: $ip/" overrides.yml
+sed -i "s/TQ_S3_STATIC_HOST: .*/TQ_S3_STATIC_HOST: $ip/" overrides.yml
+sed -i "s/TQ_S3_POSTFINANCE_HOST: .*/TQ_S3_POSTFINANCE_HOST: $ip/" overrides.yml
 ./scripts/generate_env.py # regenerate env to include the new overrides
 
 echo "Finishing initialisation..."
