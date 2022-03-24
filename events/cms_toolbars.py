a887b492060c4fc51d53ed8168c15103eec89de8
Daniel Sparber <daniel@sparber.io>
2022-03-24 17:28:12 +0100

    Added event registration and event category to toolbar

--- events/cms_toolbars.py
+++ events/cms_toolbars.py
@@ -6,8 +6,14 @@ from cms.toolbar_base import CMSToolbar
 
 @toolbar_pool.register
 class EventsToolbar(CMSToolbar):
-    def populate(self):
+    def populate(self) -> None:
         menu = self.toolbar.get_or_create_menu('events-app', _('Events'))
         if self.request.user.has_perm('events.change_event'):
             url = reverse('admin:events_event_changelist')
             menu.add_sideframe_item(_('Events'), url=url)
+        if self.request.user.has_perm('events.change_eventregistration'):
+            url = reverse('admin:events_event_eventregistration_changelist')
+            menu.add_sideframe_item(_('Event Registrations'), url=url)
+        if self.request.user.has_perm('events.change_eventcategory'):
+            url = reverse('admin:events_event_eventcategory_changelist')
+            menu.add_sideframe_item(_('Event Categories'), url=url)
