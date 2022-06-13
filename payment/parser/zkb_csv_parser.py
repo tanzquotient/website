cd018a05132cc87b72fda264079f0415fa68f1c1
Daniel Sparber <daniel@sparber.io>
2022-06-13 13:58:50 +0200

    ZKB has new csv format

--- payment/parser/zkb_csv_parser.py
+++ payment/parser/zkb_csv_parser.py
@@ -31,33 +31,35 @@ class ZkbCsvParser:
         reader = csv.DictReader(content, delimiter=";")
 
         payments = []
-        payment = None
+
         for row in reader:
-            if row['Datum']:  # For some weird reason, csv entries may span two lines, but date is always on first line
+            payment = Payment()
+            payment.file = finance_file
+            payment.filename = csv_file.name
+            payment.date = datetime.strptime(row['Datum'], "%d.%m.%Y")
+            payment.transaction_id = row['ZKB-Referenz']
+            payment.currency_code = 'CHF'
+            payment.remittance_user_string = row['Zahlungszweck']
+            payment.comment = row['Buchungstext']
 
-                # Save previous payment if it exists and has not been saved already
-                if payment is not None and not Payment.objects.filter(transaction_id=payment.transaction_id).exists():
-                    payment.save()
-                    payments.append(payment)
+            # Parse name and address from details
+            details: str = row['Details']
+            parts = details.split(',')
+            payment.name = parts[0] if len(parts) > 0 else ''
+            payment.address = ','.join(parts[1:]) if len(parts) > 1 else ''
 
-                payment = Payment()
-                payment.file = finance_file
-                payment.filename = csv_file.name
-                payment.date = datetime.strptime(row['Datum'], "%d.%m.%Y")
-                payment.transaction_id = row['ZKB-Referenz']
-                payment.currency_code = 'CHF'
-                if row['Gutschrift CHF']:
-                    payment.credit_debit = CreditDebit.CREDIT
-                    payment.amount = Decimal(row['Gutschrift CHF'])
-                else:
-                    payment.credit_debit = CreditDebit.DEBIT
-                    payment.amount = Decimal(row['Belastung CHF'])
-            elif payment is not None:
-                text: str = row['Buchungstext']
-                parts = text.split(',')
-                payment.name = parts[0] if len(parts) > 0 else ''
-                payment.address = ','.join(parts[1:]) if len(parts) > 1 else ''
-                payment.remittance_user_string = row['Zahlungszweck']
+            # Read amount from corresponding fields
+            if row['Gutschrift CHF']:
+                payment.credit_debit = CreditDebit.CREDIT
+                payment.amount = Decimal(row['Gutschrift CHF'])
+            else:
+                payment.credit_debit = CreditDebit.DEBIT
+                payment.amount = Decimal(row['Belastung CHF'])
+
+            # Save payment only if it has not been saved before
+            if not Payment.objects.filter(transaction_id=payment.transaction_id).exists():
+                payment.save()
+                payments.append(payment)
 
         finance_file.processed = True
         finance_file.save()
