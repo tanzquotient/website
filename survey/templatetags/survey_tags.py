90d548f49ebf1de92f3b962bc20db29c93cb5788
Daniel Sparber <daniel@sparber.io>
2023-03-22 22:11:53 +0100

    WIP: started UI for hiding answers

--- survey/templatetags/survey_tags.py
+++ survey/templatetags/survey_tags.py
@@ -1,18 +1,28 @@
 from django import template
+from django.contrib.auth.models import User
 
 from courses.models import Offering, Course
-from ..models import Question, Survey
+from ..models import Question, Survey, Answer
 from ..utils import get_plot_for_question, get_free_form_answers_for_question
 
 register = template.Library()
 
 
 @register.inclusion_tag(filename='survey/components/question_result.html')
-def survey_question(question: Question, selected_offering: Offering, selected_course: Course):
+def survey_question(question: Question, selected_offering: Offering, selected_course: Course, user: User):
     return dict(
+        user=user,
         question=question,
         plot=get_plot_for_question(question, selected_offering, selected_course),
         free_form_answers=get_free_form_answers_for_question(question, selected_offering, selected_course),
+    )
+
+
+@register.inclusion_tag(filename='survey/components/question_result_free_form_answer.html')
+def free_form_answer(answer: str, user: User):
+    return dict(
+        user=user,
+        answer=dict(value=answer, id=len(answer), hidden=len(answer) > 100, hidden_by=user),
     )
 
 
