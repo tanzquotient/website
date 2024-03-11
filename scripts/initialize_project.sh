4f815c9d8a2b064b4b481f2ce646ae7697202c1c
Thore Goebel <thgoebel@student.ethz.ch>
2024-03-11 09:57:22 +0100

    Remove old minio IP workaround
    This workaround was needed back when we ran Django in a container
    in development, too. Nowadays we run Django in a normal venv,
    and only use containers for Postgres and Minio.
    
    Back then, the problem was that both the Django container and your browser
    would need to connect to Minio. From Django, only "tq-data" and "172.x.x.x"
    would be valid ways (but not localhost). From your browser, localhost and
    172.x.x.x would work, but not tq-data (the container name of minio).
    
    Now that both Django and your browser run directly on your laptop, we can
    simply use localhost. Or rather to avoid issues, 127.0.0.1.

--- scripts/initialize_project.sh
+++ scripts/initialize_project.sh
@@ -9,22 +9,8 @@ pip install -r requirements.txt
 python scripts/generate_env_override.py
 python scripts/generate_env.py
 
-# Workaround for local minio development
-#
-# The problem is that "localhost" is not a valid value from the perspective
-# of the Django container accessing Minio, and "tq-data" is not a valid value
-# from the perspective of your browser accessing Minio.
 docker compose up -d
 
-echo "Setting IPs for Minio..."
-ip=$(docker inspect tq-data | grep IPAddress | grep --extended-regexp --only-matching "([0-9]{1,3}\.){3}[0-9]{1,3}")
-echo "Using IP: $ip"
-sed -i "s/TQ_S3_MEDIA_HOST: .*/TQ_S3_MEDIA_HOST: $ip/" overrides.yml
-sed -i "s/TQ_S3_STATIC_HOST: .*/TQ_S3_STATIC_HOST: $ip/" overrides.yml
-sed -i "s/TQ_S3_FINANCE_HOST: .*/TQ_S3_FINANCE_HOST: $ip/" overrides.yml
-python scripts/generate_env.py # regenerate env to include the new overrides
-
-echo "Finishing initialisation..."
 mkdir -p logs
 
 # Initialize minio
