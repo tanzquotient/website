ed0cd2c8b846ae5284d8f81c5603c0a749bce6a7
Lorenzo Petrella <lpetrella@ethz.ch>
2024-03-19 16:39:43 +0100

    added "empty" nationality option on user profile edit page

--- courses/forms/user_edit_from.py
+++ courses/forms/user_edit_from.py
@@ -63,7 +63,7 @@ class UserEditForm(forms.Form):
     about_me.help_text = _(
         "Tipp: Hier kann auch HTML Code zum Formatieren eingegeben werden!"
     )
-    nationality = LazyTypedChoiceField(choices=countries, required=False)
+    nationality = LazyTypedChoiceField(choices=[("", "Select country")] + list(countries), required=False)
     residence_permit = forms.ChoiceField(choices=Residence.CHOICES, required=False)
     ahv_number = forms.CharField(max_length=255, required=False)
     zemis_number = forms.CharField(max_length=255, required=False)
@@ -71,7 +71,7 @@ class UserEditForm(forms.Form):
     bank_name = forms.CharField(max_length=255, required=False)
     bank_zip_code = forms.CharField(max_length=255, required=False)
     bank_city = forms.CharField(max_length=255, required=False)
-    bank_country = LazyTypedChoiceField(choices=countries, required=False)
+    bank_country = LazyTypedChoiceField(choices=[("", "Select country")] + list(countries), required=False)
 
     def clean(self) -> dict:
         cleaned_data = super(UserEditForm, self).clean()
