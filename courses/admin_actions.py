9d9d0a8fef2296a63aa187bb8726e8655403a05a
Lorenzo Petrella <lpetrella@ethz.ch>
2024-04-19 14:59:21 +0200

    added initial value to comment field

--- courses/admin_actions.py
+++ courses/admin_actions.py
@@ -262,7 +262,8 @@ def send_vouchers_for_subscriptions(modeladmin, request, queryset):
     if not form:
         form = SendVoucherForm(
             initial={
-                "_selected_action": map(str, queryset.values_list("id", flat=True))
+                "_selected_action": map(str, queryset.values_list("id", flat=True)),
+                "voucher_comment": f"Sent to subscriptions to: {', '.join(list(set([subscription.course.name for subscription in queryset])))}"
             }
         )
 
