85d3837462da365091c24c5e0fcb98a826813191
Lorenzo Petrella <lpetrella@ethz.ch>
2024-05-02 22:31:08 +0200

    raise 404 if trying to download qr-bill of paid subscription

--- payment/views/subscription_qr_bill_export_pdf.py
+++ payment/views/subscription_qr_bill_export_pdf.py
@@ -1,6 +1,6 @@
 from tempfile import TemporaryFile
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
 
 from courses.models import Subscribe
@@ -10,11 +10,15 @@ from payment.utils import create_qrbill_for_subscription, to_pdf
 def subscription_qr_bill_export_pdf(request: HttpRequest, usi: str):
     subscription = get_object_or_404(Subscribe, usi=usi)
 
-    with TemporaryFile() as pdf_file:
-        to_pdf(create_qrbill_for_subscription(subscription), pdf_file)
-        length = pdf_file.tell()
-        pdf_file.seek(0)
-        response = HttpResponse(pdf_file, content_type="application/pdf")
-        response["Content-Disposition"] = f"attachment; filename=QR-bill_{usi}.pdf"
-        response["Content-Length"] = length
-        return response
+    if subscription.paid or subscription.open_amount.is_zero:
+        raise Http404
+
+    else:
+        with TemporaryFile() as pdf_file:
+            to_pdf(create_qrbill_for_subscription(subscription), pdf_file)
+            length = pdf_file.tell()
+            pdf_file.seek(0)
+            response = HttpResponse(pdf_file, content_type="application/pdf")
+            response["Content-Disposition"] = f"attachment; filename=QR-bill_{usi}.pdf"
+            response["Content-Length"] = length
+            return response
