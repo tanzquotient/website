9bc288915d56b084301a1b334c63687f1a546b77
Lorenzo Petrella <lpetrella@ethz.ch>
2024-05-10 17:12:13 +0200

    edited send group email action to queue

--- email_system/admin/admin_actions.py
+++ email_system/admin/admin_actions.py
@@ -1,22 +1,22 @@
-from django.contrib import messages
+from django.contrib import admin, messages
 from django.contrib.messages import WARNING
 
 from email_system.services import send_group_email, copy_group_email
+from email_system.models.choices import GroupEmailState
 
 
+@admin.action(description="Send selected emails")
 def send_emails_admin_action(modeladmin, request, queryset):
     for email in queryset:
-        if email.is_sent():
+        if email.state != GroupEmailState.DRAFT:
             messages.add_message(
                 request,
                 WARNING,
-                'Skipping "{}". This email has already been sent'.format(email),
+                f"Skipping {email}. You can only send emails in a draft state.",
             )
         else:
-            send_group_email(email)
-
-
-send_emails_admin_action.short_description = "Send selected emails"
+            email.state = GroupEmailState.QUEUED
+            email.save()
 
 
 def copy_emails_admin_action(modeladmin, request, queryset):
