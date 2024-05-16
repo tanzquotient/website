2df1d7aa4b4ee4026de3bc35bf05b17a317e942c
Lorenzo Petrella <lpetrella@ethz.ch>
2024-05-17 00:10:44 +0200

    code cleanup

--- email_system/admin/admin_actions.py
+++ email_system/admin/admin_actions.py
@@ -1,27 +1,23 @@
-from django.contrib import messages
+from django.contrib import messages, admin
 from django.contrib.messages import WARNING
 
 from email_system.services import send_group_email, copy_group_email
 
 
+@admin.action(description="Send selected emails")
 def send_emails_admin_action(modeladmin, request, queryset):
     for email in queryset:
         if email.is_sent():
             messages.add_message(
                 request,
                 WARNING,
-                'Skipping "{}". This email has already been sent'.format(email),
+                f"Skipping {email}. This email has already been sent",
             )
         else:
             send_group_email(email)
 
 
-send_emails_admin_action.short_description = "Send selected emails"
-
-
+@admin.action(description="Copy selected emails")
 def copy_emails_admin_action(modeladmin, request, queryset):
     for email in queryset:
         copy_group_email(email)
-
-
-copy_emails_admin_action.short_description = "Copy selected emails"
