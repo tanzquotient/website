cefe18c32832bbcb9f5317260da8c378aef528d5
Lorenzo Petrella <lpetrella@ethz.ch>
2024-05-17 00:51:50 +0200

    updated GroupEmail admin table

--- email_system/admin/group_email_admin.py
+++ email_system/admin/group_email_admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import timezone
 from parler.admin import TranslatableAdmin
 
 from email_system.admin.admin_actions import (
@@ -6,13 +7,13 @@ from email_system.admin.admin_actions import (
     copy_emails_admin_action,
 )
 from email_system.models import GroupEmail
-
+from post_office.models import STATUS as EmailStatus
 
 @admin.register(GroupEmail)
 class GroupEmailAdmin(TranslatableAdmin):
     model = GroupEmail
 
-    list_display = ["subject", "target_group", "sent_at"]
+    list_display = ["subject", "target_group", "dispatched_at", "status"]
     list_filter = ["target_group"]
     search_fields = ["target_group__name"]
     actions = [send_emails_admin_action, copy_emails_admin_action]
@@ -22,3 +23,18 @@ class GroupEmailAdmin(TranslatableAdmin):
         if obj is not None:
             return not obj.is_sent()
         return True
+
+    def dispatched_at(self, group_email: GroupEmail) -> str:
+        if not group_email.generated_emails.exists():
+            return None
+        else:
+            return timezone.localtime(group_email.generated_emails.first().email.created).strftime(
+            "%d %b %Y %H:%M:%S"
+        )
+
+    def status(self, group_email: GroupEmail) -> str:
+        if not group_email.generated_emails.exists():
+            return None
+        else:
+            generated_emails = group_email.generated_emails
+            return f"{generated_emails.filter(email__status=EmailStatus.sent).count()}/{generated_emails.count()} sent"
