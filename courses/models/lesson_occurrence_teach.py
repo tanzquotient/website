d580c7dd8f11b05e62d6a2df377fd4132a8f8db2
Lorenzo Petrella <lpetrella@ethz.ch>
2024-06-04 01:06:46 +0200

    adjust LessonOccurrenceTeach hourly wage if theacher is main teacher

--- courses/models/lesson_occurrence_teach.py
+++ courses/models/lesson_occurrence_teach.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 from django.db import models
 from django.contrib.auth.models import User
 
+from courses.models import Teach
+
 
 class LessonOccurrenceTeach(models.Model):
     lesson_occurrence = models.ForeignKey(to="LessonOccurrence", on_delete=models.CASCADE)
@@ -23,6 +25,10 @@ class LessonOccurrenceTeach(models.Model):
         # For external courses, teachers are not paid by us
         if self.lesson_occurrence.course.is_external():
             return Decimal(0)
+        # Main teachers get the hourly wage set at the creation of the course
+        elif Teach.objects.filter(course=self.lesson_occurrence.course, teacher=self.teacher).exists():
+            return Teach.objects.get(course=self.lesson_occurrence.course, teacher=self.teacher).hourly_wage
+        # Compute the hourly wage for substitute teachers
         else:
             return self.teacher.profile.get_hourly_wage()
 
