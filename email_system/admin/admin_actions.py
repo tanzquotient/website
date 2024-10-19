777bffa74e927024d598a9ad2555e159edbe9f7e
Lorenzo Petrella <lpetrella@ethz.ch>
2024-10-19 18:00:08 +0200

    deprecate instant group email sending

--- email_system/admin/admin_actions.py
+++ email_system/admin/admin_actions.py
@@ -5,20 +5,6 @@ from django.contrib.messages import WARNING
 from email_system.services import send_group_email, copy_group_email
 
 
-@admin.action(description="Send selected emails")
-def send_emails_admin_action(modeladmin, request, queryset):
-    for email in queryset:
-        if email.is_dispatched():
-            messages.add_message(
-                request,
-                WARNING,
-                f"Skipping {email}. This email has already been sent",
-            )
-        else:
-            with transaction.atomic():
-                send_group_email(email)
-
-
 @admin.action(description="Copy selected emails")
 def copy_emails_admin_action(modeladmin, request, queryset):
     for email in queryset:
