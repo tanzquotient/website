3d6b9e48841d14e91b61974e435b47b68a19e280
Lorenzo Petrella <lpetrella@ethz.ch>
2024-11-24 12:57:58 +0100

    custom migration to set default review and makemigrations

---
+++ survey/migrations/0005_survey_instance_default_expiration_date.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+from survey.models import SurveyInstance
+from datetime import timedelta
+
+
+def resize(apps, schema_editor):
+    instances_to_update = SurveyInstance.objects.filter(
+        url_expire_date__isnull=True
+    ).all()
+    for instance_to_update in instances_to_update:
+        instance_to_update.url_expire_date = instance_to_update.date + timedelta(
+            days=90
+        )
+        instance_to_update.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("survey", "0004_remove_surveyinstance_invitation_sent"),
+    ]
+
+    operations = [
+        migrations.RunPython(resize),
+    ]
