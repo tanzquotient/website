38962417bf8d87cd06b57c45e7bea8256e2f5cb3
Lorenzo Petrella <lpetrella@ethz.ch>
2024-11-24 13:51:29 +0100

    kick faulty migration that didn't work anyway

--- courses/migrations/0029_alter_offeringtranslation_title.py
+++ courses/migrations/0029_alter_offeringtranslation_title.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("courses", "0028_resize_userprofile_picture"),
+        ("courses", "0027_alter_userprofile_picture"),
     ]
 
     operations = [
