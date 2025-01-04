e739f84e9ad6acc76056b5fd453d02e255a12aa3
lpetrella <lpetrella@student.ethz.ch>
2025-01-04 17:58:26 +0000

    improved teachers' hours lookup

--- courses/services/data/teachers_overview.py
+++ courses/services/data/teachers_overview.py
@@ -1,82 +1,45 @@
-from collections import defaultdict
-from decimal import Decimal
-from typing import Iterable
-
-from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
-
-from courses.models import Teach
+from courses.models import LessonOccurrence
+from decimal import Decimal
 
 
 def get_teachers_overview_data() -> list[list]:
-    grouped_teachings = defaultdict(lambda: defaultdict(list))
-    for teaching in (
-        Teach.objects.order_by("teacher__first_name", "teacher__last_name")
-        .prefetch_related(
-            "teacher",
-            "course__irregular_lessons",
-            "course__regular_lessons",
-            "course__regular_lessons__exceptions",
-            "course__offering__period",
-            "course__offering__period__cancellations",
-            "course__period",
-            "course__period__cancellations",
-            "course__room__cancellations",
-        )
+    lesson_occurrences = (
+        LessonOccurrence.objects.prefetch_related("teachers", "course")
+        .exclude(course__cancelled=True)
+        .order_by("start")
         .all()
-    ):
-        if (
-            teaching.course.is_external()
-            or teaching.course.cancelled
-            or not teaching.course.get_lessons()
-        ):
+    )
+    years = range(
+        lesson_occurrences.first().start.year,
+        lesson_occurrences.last().start.year + 1,
+    )
+    header = (
+        [_("First name"), _("Last name")]
+        + list(years)
+        + [_("Total years"), _("Total hours")]
+    )
+    teachers_data = dict()
+    for l in lesson_occurrences:
+        if l.course.is_external():
             continue
-
-        first_lesson_date = teaching.course.get_first_lesson_date()
-        year = str(first_lesson_date.year) if first_lesson_date else _("unknown year")
-        grouped_teachings[teaching.teacher_id][year].append(teaching)
+        for t in l.teachers.all():
+            teachers_data.setdefault(
+                t,
+                {y: Decimal(0) for y in years},
+            )
+            teachers_data[t][l.start.year] += l.get_hours()
 
     rows = []
-
-    years = sorted(
-        {year for group in grouped_teachings.values() for year in group.keys()}
-    )
-
-    header = (
-        [_("First name"), _("Last name")] + years + [_("Total years"), _("Total hours")]
-    )
-
-    for grouped_by_year in grouped_teachings.values():
-        teacher: User = list(grouped_by_year.values())[0][0].teacher
-        row = [teacher.first_name, teacher.last_name]
-
-        for year in years:
-            row.append(sum_of_course_hours(grouped_by_year.get(year, [])) or "-")
-
-        # Number of years the teacher has been teaching
-        # (counting unknown year as one year)
-        row.append(len(grouped_by_year.values()))
-
-        row.append(
-            sum_of_course_hours(
-                [
-                    teaching
-                    for teachings in grouped_by_year.values()
-                    for teaching in teachings
-                ]
-            )
+    for t, t_data in teachers_data.items():
+        rows.append(
+            [t.first_name, t.last_name]
+            + [round(t_data[y], 2) or "-" for y in years]
+            + [
+                len(years) - list(t_data.values()).count(Decimal(0)),
+                round(sum(t_data.values(), Decimal(0)), 2),
+            ]
         )
 
-        rows.append(row)
-
-    # Sort by last column (total hours) descending
-    sorted_rows = sorted(rows, key=lambda r: r[-1], reverse=True)
-
+    sorted_rows = sorted(rows, key=lambda r: (r[-1]), reverse=True)
     return [header] + sorted_rows
-
-
-def sum_of_course_hours(teachings: Iterable[Teach]) -> Decimal:
-    return sum(
-        [teaching.course.get_total_hours() for teaching in teachings],
-        Decimal(0),
-    )
