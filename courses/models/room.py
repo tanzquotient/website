b3a077cfb19dec19cdbf36af589bd71b263a4adc
Daniel Sparber <daniel@sparber.io>
2025-02-08 21:57:19 +0100

    Added additional text fields to room

--- courses/models/room.py
+++ courses/models/room.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from djangocms_text.fields import HTMLField
 from parler.models import TranslatableModel, TranslatedFields
 
@@ -16,32 +19,69 @@ class Room(TranslatableModel):
     contact_info = models.TextField(blank=True, null=True)
 
     translations = TranslatedFields(
-        description=HTMLField(verbose_name="[TR] Description", blank=True, null=True),
+        description=HTMLField(
+            verbose_name="[TR] Description",
+            blank=True,
+            null=True,
+            help_text=_(
+                "Information on how to get to the room. E.g. where it is located, "
+                "what entrance to use, or any additional information."
+            ),
+        ),
         instructions=models.TextField(
+            verbose_name="[TR] Disclaimer",
+            blank=True,
+            null=True,
+            help_text=_(
+                "Instructions for teachers/staff. E.g. how to get access, how to use "
+                "the sound system, noise regulations, etc."
+            ),
+        ),
+        disclaimer=models.TextField(
             verbose_name="[TR] Instructions",
             blank=True,
             null=True,
-            help_text="Instructions to prepare the room (for teachers/staff only)",
+            help_text=_(
+                "Important notes for this room, that will be shown together with the "
+                "course description. E.g. required ASVZ membership, notes on "
+                "last minute cancellations for food&lab, etc."
+            ),
+        ),
+        information_for_participants=HTMLField(
+            verbose_name="[TR] Information for participants",
+            blank=True,
+            null=True,
+            help_text=_(
+                "Shown only to participants who are confirmed for a course in this room."
+            ),
         ),
     )
 
-    def get_description(self):
+    def get_description(self) -> Optional[str]:
         return TranslationUtils.get_text_with_language_fallback(self, "description")
 
-    def get_instructions(self):
+    def get_instructions(self) -> Optional[str]:
         return TranslationUtils.get_text_with_language_fallback(self, "instructions")
 
-    def has_additional_info(self):
+    def get_disclaimer(self) -> Optional[str]:
+        return TranslationUtils.get_text_with_language_fallback(self, "disclaimer")
+
+    def get_information_for_participants(self) -> Optional[str]:
+        return TranslationUtils.get_text_with_language_fallback(
+            self, "information_for_participants"
+        )
+
+    def has_additional_info(self) -> bool:
         return (
             self.address
             or self.url
             or self.contact_info
             or self.get_description()
             or self.get_instructions()
-        )
+        ) is not None
 
-    def __str__(self):
-        return "{}".format(self.name)
+    def __str__(self) -> str:
+        return self.name
 
     class Meta:
         ordering = ["name"]
