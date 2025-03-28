5616b58b82e5baf8192448ca3fb535ba494c9bb9
Daniel Sparber <daniel@sparber.io>
2025-03-28 11:51:00 +0100

    Added attendance model

---
+++ courses/models/attendance.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db.models import ForeignKey, CharField, Model, CASCADE
+
+from .choices import AttendanceState
+
+
+class Attendance(Model):
+    user = ForeignKey(to=User, related_name="attendances", on_delete=CASCADE)
+    lesson_occurrence = ForeignKey(
+        to="LessonOccurrence", related_name="attendances", on_delete=CASCADE
+    )
+    state = CharField(
+        max_length=24,
+        blank=False,
+        null=False,
+        choices=AttendanceState.CHOICES,
+        default=AttendanceState.PRESENT,
+    )
+
+    class Meta:
+        unique_together = (("lesson_occurrence", "user"),)
+
+    def __str__(self) -> str:
+        return (
+            f"{self.lesson_occurrence}, {self.user.get_full_name()}: "
+            f"{AttendanceState.NAMES[self.state]}"
+        )
---
+++ courses/models/choices/attendance_state.py
@@ -0,0 +1,23 @@
+from django.utils.translation import gettext_lazy as _
+
+
+class AttendanceState:
+    PRESENT = "p"
+    ABSENT_EXCUSED = "absent_excused"
+    ABSENT_NOT_EXCUSED = "absent_not_excused"
+    REPLACEMENT_CONFIRMED = "replacement_confirmed"
+    REPLACEMENT_PENDING = "replacement_pending"
+
+    CHOICES = (
+        (PRESENT, _("present")),
+        (ABSENT_EXCUSED, _("absent, excused")),
+        (ABSENT_NOT_EXCUSED, _("absent, not excused")),
+        (REPLACEMENT_CONFIRMED, _("replacement confirmed")),
+        (REPLACEMENT_PENDING, _("replacement pending")),
+    )
+
+    NAMES = {key: name for key, name in CHOICES}
+
+    PRESENT_STATES = [PRESENT, REPLACEMENT_CONFIRMED]
+    ABSENT_STATES = [ABSENT_EXCUSED, ABSENT_NOT_EXCUSED]
+    PENDING_STATES = [REPLACEMENT_PENDING]
