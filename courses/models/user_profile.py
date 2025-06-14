abfd10172f2becf9f69a1e9d39c89d9462d72198
Lorenzo Petrella <lpetrella@ethz.ch>
2025-06-14 19:19:04 +0200

    refactor UserProfile methods

--- courses/models/user_profile.py
+++ courses/models/user_profile.py
@@ -153,20 +153,113 @@ class UserProfile(Model):
         except UserProfile.DoesNotExist:
             pass
 
-    # convenience method for model user are added here
-    def is_teacher(self) -> bool:
-        return self.user.teaching_courses.exists()
+    ###########
+    # General #
+    ###########
 
     def get_display_name(self) -> str:
         if not self.display_name or not self.is_teacher():
             return self.user.get_full_name()
         return self.display_name
 
-    def is_substitute_teacher(self) -> bool:
-        return not self.is_teacher() and self.user.lesson_occurrences.exists()
+    def is_teacher(self) -> bool:
+        return self.user.teaching_courses.exists()
 
     def is_student(self) -> bool:
         return self.student_status in StudentStatus.STUDENTS and self.legi
+
+    def is_board_member(self) -> bool:
+        return self.user.functions.count() > 0
+
+    #################
+    # Subscriptions #
+    #################
+
+    @cached_property
+    def subscriptions(self) -> Iterable[Subscribe]:
+        return self.user.subscriptions.order_by("-date").all()
+
+    def subscriptions_with_overdue_payment(self) -> Iterable[Subscribe]:
+        return [
+            subscription
+            for subscription in self.subscriptions
+            if subscription.is_payment_overdue()
+        ]
+
+    def unpaid_subscriptions(self) -> Iterable[Subscribe]:
+        return [
+            subscription
+            for subscription in self.subscriptions
+            if subscription.state in SubscribeState.TO_PAY_STATES
+        ]
+
+    def get_subscribed_courses(self) -> Iterable[Course]:
+        return [s.course for s in self.subscriptions]
+
+    def get_active_subscribed_course_ids(self) -> list[int]:
+        return [s.course_id for s in self.subscriptions if s.is_active()]
+
+    def get_waiting_list_course_ids(self) -> list[int]:
+        return [
+            s.course_id
+            for s in self.subscriptions
+            if s.state == SubscribeState.WAITING_LIST
+        ]
+
+    def get_past_subscriptions(self) -> Iterable[Subscribe]:
+        sql = (
+            "SELECT * FROM courses_subscribe "
+            "WHERE user_id = %s AND course_id IN (SELECT id FROM past_courses) "
+            "ORDER BY id DESC"
+        )
+        return Subscribe.objects.raw(sql, [self.user_id])
+
+    def get_current_subscriptions(self) -> Iterable[Subscribe]:
+        sql = (
+            "SELECT * FROM courses_subscribe "
+            "WHERE user_id = %s AND course_id IN (SELECT id FROM current_courses) "
+            "ORDER BY id DESC"
+        )
+        return Subscribe.objects.raw(sql, [self.user_id])
+
+    def get_student_status(self) -> str:
+        return StudentStatus.TEXT[self.student_status]
+
+    def get_residence_permit(self) -> str:
+        return Residence.PERMITS[self.residence_permit]
+
+    def get_nationality(self) -> str:
+        return COUNTRIES[self.nationality]
+
+    def is_complete(self) -> bool:
+        return not self.missing_values()
+
+    def missing_values(self) -> Iterable[str]:
+        if self.is_teacher():
+            missing = []
+            if not self.birthdate:
+                missing.append("birth date")
+            if not self.residence_permit:
+                missing.append("residence permit")
+            if not self.ahv_number:
+                missing.append("AHV number")
+            # Every registered foreigner in switzerland gets a ZEMIS number,
+            # thus applicable for everyone except swiss
+            # and non-registered people (e.g. short term stay withoutt need for visa)
+            if not self.zemis_number and self.residence_permit not in ["SWISS", "<NO>"]:
+                missing.append("ZEMIS number")
+            if not self.bank_account:
+                missing.append("bank account")
+
+            return missing
+        return []
+
+    ############
+    # Teachers #
+    ############
+
+    def is_substitute_teacher(self) -> bool:
+        return not self.is_teacher() and self.user.lesson_occurrences.exists()
 
     def get_hourly_wage(self, until: datetime | str = "now") -> Decimal:
         # If a teacher has a fixed wage, return it
@@ -239,9 +332,6 @@ class UserProfile(Model):
             and teaching.course.is_over()
         }
 
-    def is_board_member(self) -> bool:
-        return self.user.functions.count() > 0
-
     def get_styles_of_teacher(self) -> set[CourseType]:
         if not self.is_teacher():
             return set()
@@ -251,45 +341,6 @@ class UserProfile(Model):
             styles.update(set(teaching.course.type.styles.all()))
 
         return styles
-
-    @cached_property
-    def subscriptions(self) -> Iterable[Subscribe]:
-        return self.user.subscriptions.order_by("-date").all()
-
-    def subscriptions_with_overdue_payment(self) -> Iterable[Subscribe]:
-        return [
-            subscription
-            for subscription in self.subscriptions
-            if subscription.is_payment_overdue()
-        ]
-
-    def unpaid_subscriptions(self) -> Iterable[Subscribe]:
-        return [
-            subscription
-            for subscription in self.subscriptions
-            if subscription.state in SubscribeState.TO_PAY_STATES
-        ]
-
-    def get_subscribed_courses(self) -> Iterable[Course]:
-        return [s.course for s in self.subscriptions]
-
-    def get_active_subscribed_course_ids(self) -> list[int]:
-        return [s.course_id for s in self.subscriptions if s.is_active()]
-
-    def get_waiting_list_course_ids(self) -> list[int]:
-        return [
-            s.course_id
-            for s in self.subscriptions
-            if s.state == SubscribeState.WAITING_LIST
-        ]
-
-    def get_past_subscriptions(self) -> Iterable[Subscribe]:
-        sql = (
-            "SELECT * FROM courses_subscribe "
-            "WHERE user_id = %s AND course_id IN (SELECT id FROM past_courses) "
-            "ORDER BY id DESC"
-        )
-        return Subscribe.objects.raw(sql, [self.user_id])
 
     def get_current_teaching_courses(self) -> Iterable[Course]:
         courses = [
@@ -320,45 +371,6 @@ class UserProfile(Model):
         )
         courses.sort(key=Course.get_first_lesson_date)
         return courses
-
-    def get_current_subscriptions(self) -> Iterable[Subscribe]:
-        sql = (
-            "SELECT * FROM courses_subscribe "
-            "WHERE user_id = %s AND course_id IN (SELECT id FROM current_courses) "
-            "ORDER BY id DESC"
-        )
-        return Subscribe.objects.raw(sql, [self.user_id])
-
-    def get_student_status(self) -> str:
-        return StudentStatus.TEXT[self.student_status]
-
-    def get_residence_permit(self) -> str:
-        return Residence.PERMITS[self.residence_permit]
-
-    def get_nationality(self) -> str:
-        return COUNTRIES[self.nationality]
-
-    def is_complete(self) -> bool:
-        return not self.missing_values()
-
-    def missing_values(self) -> Iterable[str]:
-        if self.is_teacher():
-            missing = []
-            if not self.birthdate:
-                missing.append("birth date")
-            if not self.residence_permit:
-                missing.append("residence permit")
-            if not self.ahv_number:
-                missing.append("AHV number")
-            # Every registered foreigner in switzerland gets a ZEMIS number, thus applicable for everyone except swiss
-            # and non-registered people (e.g. short term stay withoutt need for visa)
-            if not self.zemis_number and self.residence_permit not in ["SWISS", "<NO>"]:
-                missing.append("ZEMIS number")
-            if not self.bank_account:
-                missing.append("bank account")
-
-            return missing
-        return []
 
     class Meta:
         permissions = (("access_counterpayment", "Can access counter payment"),)
