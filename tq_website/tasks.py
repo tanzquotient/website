da31e9b1e777be259ca562e86d3fc7fd04cfcff2
Lorenzo Petrella <lpetrella@ethz.ch>
2025-07-19 23:15:18 +0200

    automatically send reminders upon finance file upload

--- tq_website/tasks.py
+++ tq_website/tasks.py
@@ -6,6 +6,7 @@ from groups.services import update_groups
 from survey.services import send_course_surveys
 from payment.payment_processor import PaymentProcessor
 from payment.parser import ZkbCsvParser
+from payment.services import remind_all_of_payments
 from courses.services.teachers import send_presence_reminder
 from email_system.services import send_scheduled_group_emails
 from photologue.models import Photo, PhotoSizeCache
@@ -26,8 +27,12 @@ def payment_parse_zkb_csv_files() -> None:
 
 
 @shared_task(name="payment_match", ignore_result=True)
-def match_payments() -> None:
+def match_payments(send_reminders: bool = False) -> None:
     PaymentProcessor().process_payments()
+    if send_reminders:
+        remind_all_of_payments(
+            min_days_from_course_start=7, min_days_from_previous_reminder=7
+        )
 
 
 @shared_task(name="update_groups", ignore_result=True)
@@ -48,6 +53,7 @@ def task_send_course_presence_reminder() -> None:
 @shared_task(name="send_scheduled_group_emails", ignore_result=True)
 def task_send_scheduled_group_emails() -> None:
     send_scheduled_group_emails()
+
 
 @shared_task(name="pre_cache_photologue_image_sizes", ignore_result=True)
 def task_pre_cache_photologue_image_sizes() -> None:
