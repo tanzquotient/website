dff9a9b091adc387e1df8d0ce96a5275f5f6e482
Daniel Sparber <daniel@sparber.io>
2025-11-22 18:48:33 +0300

    Cancellation until 3h before course start

--- courses/utils/replacements/replacement_utils.py
+++ courses/utils/replacements/replacement_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 
 from pytz import timezone
 
@@ -8,12 +8,13 @@ from courses.models import LessonOccurrence
 
 
 def change_attendance_deadline(lesson_occurrence: LessonOccurrence) -> datetime:
-    lesson_date = lesson_occurrence.start.date()
-    return datetime.combine(lesson_date, time(hour=3), tzinfo=TIMEZONE)
+    return lesson_occurrence.start - timedelta(hours=3)
 
 
 def claiming_spot_window_open(lesson_occurrence: LessonOccurrence) -> bool:
-    return datetime.now(tz=TIMEZONE) > change_attendance_deadline(lesson_occurrence)
+    lesson_date = lesson_occurrence.start.date()
+    claiming_window_start = datetime.combine(lesson_date, time(hour=3), tzinfo=TIMEZONE)
+    return datetime.now(tz=TIMEZONE) > claiming_window_start
 
 
 def change_attendance_window_open(lesson_occurrence: LessonOccurrence) -> bool:
