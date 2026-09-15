fac89fec48279f3fb461682eabb583aa67b1f01b
lpetrella <lpetrella@student.ethz.ch>
2026-09-15 20:54:24 +0200

    fix copy_event action to include translatable fields
    fixed a bug by which prefetched translatable fields were being copied to new events but therefore not saved

--- events/admin_actions.py
+++ events/admin_actions.py
@@ -6,25 +6,24 @@ from events.models import Event
 
 def copy_event(modeladmin, request, queryset) -> None:
     for event in queryset:
-        old_id = event.id
-        event.date = event.date + datetime.timedelta(days=7)
-        if event.date_to:
-            event.date_to = event.date_to + datetime.timedelta(days=7)
-        event.id = None
-        event.save()
-        old_event = Event.objects.get(id=old_id)
+        old_event = Event.objects.get(id=event.id)
+        new_event = Event.objects.get(id=event.id)
+        new_event.pk = None
+        new_event.id = None
+        new_event._state.adding = True
 
-        event.set_current_language("de")
-        old_event.set_current_language("de")
-        event.name = old_event.name
-        event.description = old_event.description
+        new_event.date = old_event.date + datetime.timedelta(days=7)
+        if old_event.date_to:
+            new_event.date_to = old_event.date_to + datetime.timedelta(days=7)
+        new_event.save()
 
-        event.set_current_language("en")
-        old_event.set_current_language("en")
-        event.name = old_event.name
-        event.description = old_event.description
+        for lang in ("de", "en"):
+            old_event.set_current_language(lang)
+            new_event.set_current_language(lang)
+            new_event.name = old_event.name
+            new_event.description = old_event.description
 
-        event.save()
+        new_event.save()
 
 
 copy_event.short_description = "Create copy of selected events (adding one week)"
